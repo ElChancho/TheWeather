73d2cc7b1a4e0399c5ed03d48d543b1c1bcec165
david <alu0101479507@ull.edu.es>
2025-02-06 12:03:21 +0000

    setup done

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App () {
+  return (
+    <>
+      <header className='font-poppins flex justify-center'>
+        <h1>ElTiempo</h1>
+      </header>
+    </>
+  )
+}
+
+export default App
