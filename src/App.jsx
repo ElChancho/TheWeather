8f2fbdae4a2d5a6fb5888d9e141e2b8a31b29946
david <alu0101479507@ull.edu.es>
2025-02-06 12:48:09 +0000

    choosing colors

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
 function App () {
   return (
     <>
-      <header className='font-poppins flex justify-center'>
-        <h1>ElTiempo</h1>
-      </header>
+      <body className='font-poppins text-white bg-prj-1'>
+        <header className='flex flex-col text-center justify-center items-center w-full'>
+          <h1>El Tiempo</h1>
+          <input type='text' placeholder='Tenerife' className='bg-prj-2 rounded-md border-white w-1/6' />
+        </header>
+
+        <main className='flex justify-center'>
+          <p>Aqui va los datos del tiempo de hoy</p>
+        </main>
+      </body>
     </>
   )
 }
